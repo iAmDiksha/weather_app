9a847b989d49fb4cd10c91cea56ad00dd7ecc1e6
Diksha <jdiksha394@gmail.com>
2023-08-21 15:44:02 +0530

    weather app created

---
+++ script.js
@@ -0,0 +1,68 @@
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
+const weatherData = document.getElementById('weather');
+const apiKey = "e51af41d27d8734610792225ced34499";
+
+searchBtn.addEventListener('click', ()=>{
+    let cityName = searchInput.value;
+    if(cityName)
+    {
+        getWeatherData(cityName);
+    }
+})
+
+const getWeatherData = (city)=>{
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(apiURL).then(res => res.json())
+    .then(data => {
+        console.log(data)
+        displayWeather(data);
+    })
+    .catch(err => console.log("Error fetching the weather data", err));
+}
+
+const displayWeather = (data)=>{
+    const weatherInfo = `<h2>${data.name}, ${data.sys.country}</h2>
+    <p>Temperature: <b>${data.main.temp}°C</b></p>
+    <p>Weather: ${data.weather[0].main}</p>
+    <p>Description: ${data.weather[0].description}</p>`;
+    weatherData.innerHTML = weatherInfo;
+
+    const weather = data.weather[0].main.toLowerCase();
+    const container = document.getElementById('container');
+    if(weather.includes('rain'))
+    {
+        container.className = 'rain';
+    }
+    else if(weather.includes('mist') || weather.includes('fog'))
+    {
+        container.className = 'mist';
+    }
+    else if(weather.includes('haze')){
+        container.className = 'haze';
+    }
+    else if(weather.includes('cloud'))
+    {
+        container.className = 'cloud';
+    }
+    else if(weather.includes('drizzle')){
+        container.className = 'drizzle';
+    }
+    else if(weather.includes('clear') || weather.includes('clean'))
+    {
+        container.className = 'clear';
+    }
+    else if(weather.includes('thunder')){
+        container.className = 'thunder';
+    }
+    else if(weather.includes('snow') || weather.includes('cold')){
+        container.className = 'snow';
+    }
+    else if(weather.includes('wind') || weather.includes('storm')){
+        container.className = 'wind';
+    }
+}
+
+
+
